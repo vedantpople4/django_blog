55c0395e0f5e1375ada31cf6bf265c58df715562
Vedant pople <vedantpople4@gmail.com>
2020-11-17 23:09:30 +0530

    new posts update

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author' : 'Vedant Pople',
+        'title' : 'Demo post',
+        'content' : 'First Post content',
+        'date' : 'October 20, 2020'
+    },
+    {
+        'author' : 'Vedant Pople Again',
+        'title' : 'Demo post 2',
+        'content' : 'First Post content 2',
+        'date' : 'October 22, 2020'
+    }
+
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
