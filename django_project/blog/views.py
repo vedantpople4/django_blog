116e2236e2304d2ef88f25c1ad9adb14d6bb2f26
Vedant pople <vedantpople4@gmail.com>
2020-12-09 16:36:10 +0530

    update admin.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author' : 'Vedant Pople',
-        'title' : 'Demo post',
-        'content' : 'First Post content',
-        'date' : 'October 20, 2020'
-    },
-    {
-        'author' : 'Vedant Pople Again',
-        'title' : 'Demo post 2',
-        'content' : 'First Post content 2',
-        'date' : 'October 22, 2020'
-    }
 
-]
 
 def home(request):
     context = {
