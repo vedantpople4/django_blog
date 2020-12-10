5445003ffa933543e7bdb97f91a314cb58a90fe8
Vedant pople <vedantpople4@gmail.com>
2020-12-10 22:30:54 +0530

    new settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-    'users.apps.UserConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -19,7 +19,7 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='registers'),
+    path('register/', user_views.register, name='register'),
     path('', include('blog.urls')),
 
 ]
