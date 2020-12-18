35d03bda7aefd1361fdd0744e8c4a1aa06c230ef
Vedant pople <vedantpople4@gmail.com>
2020-12-18 21:51:11 +0530

    update forms.py

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -9,4 +9,4 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username','email','password1','password2']
+        fields = ['username','email','password1','password2'] 
