2f9e4d2400783e7d8aa16f689a8425d57c18664e
Vedant pople <vedantpople4@gmail.com>
2020-12-22 15:48:39 +0530

    new login logout templated added

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -8,8 +8,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'\Your accoutn has been created! You can now login')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
