151ae85942d1591072766a633d9e83e2357bcbfa
Vedant pople <vedantpople4@gmail.com>
2020-12-25 20:49:58 +0530

    New post_deatils update

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', PostListView.as_view() , name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view() , name='post-detail'),
     path('about/', views.about , name='blog-about'),
-    path('post/<int:pk>/', PostDetailView.as_view() , name='post-detail')
+    
 ]
