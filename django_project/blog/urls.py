3a7fe72a75476d38e4d29a74b83258e87d5c68b8
Vedant pople <vedantpople4@gmail.com>
2020-12-27 23:49:16 +0530

    update url.py

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView, UpdateView
 from . import views
 
 urlpatterns = [
