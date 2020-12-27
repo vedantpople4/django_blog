53cdb3c8d0cf08bd1ec574af0e21ac7e9d7a9931
Vedant pople <vedantpople4@gmail.com>
2020-12-27 21:13:36 +0530

    New upadte in URLs

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', PostListView.as_view() , name='blog-home'),
-    path('user/<str:username', PostListView.as_view() , name='user-posts'),
+    path('user/<str:username/', PostListView.as_view() , name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view() , name='post-detail'),
     path('post/<int:pk>/update', PostUpdateView.as_view() , name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view() , name='post-delete'),
